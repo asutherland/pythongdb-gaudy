214f7efa7a28e1b16d80c666933bb92ece3e4613
Andrew Sutherland <asutherland@asutherland.org>
2009-09-12 03:00:05 -0700

    no idea

--- gdbaudy/mozbt.py
+++ gdbaudy/mozbt.py
@@ -50,8 +50,8 @@ class JSFrame(object):
         print '  func:', self.func_name
 
 def getfield(addr, fielddef):
-    print 'getfield', addr, fielddef, fielddef.bitpos
-    return gdb.parse_and_eval("(%s) *0x%x" % (fielddef.type,
+    print 'getfield', int(addr), fielddef, fielddef.bitpos
+    return gdb.parse_and_eval("(%s) *0x%x" % (str(fielddef.type),
                                               int(addr) + fielddef.bitpos / 8))
 
 class JSScratchContext(object):
